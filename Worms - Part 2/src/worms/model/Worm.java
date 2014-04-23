5520e3179399ae4ee90327a909676cd711822812
RubenSchroyen <ruben.kouneli@hotmail.be>
2014-04-23 12:05:43 +0200

    Edited TODO list, time to fix that

--- Worms - Part 2/src/worms/model/Worm.java
+++ Worms - Part 2/src/worms/model/Worm.java
@@ -13,13 +13,11 @@ package worms.model;
  *																																  *
  *																																  *
  *		LOW PRIORITY																											  *
- *		------------																											  *
- *		1. DOCUMENTATION 																									      *
- *			 	RALPH: Facade																				          *
+ *		------------																						          			  *
  *																																  *
- *		2. Make jump stop when hitting object or impassable 																      *
+ *		1. Make jump stop when hitting object or impassable 																      *
  *																																  *
- *		3. Worms don't spawn on the ground, but literally on an adjacent tile (is this allowed?) 								  *																	      *
+ *		2. Worms don't spawn on the ground, but literally on an adjacent tile (is this allowed?) 								  *																	      *
  *			 																													  *
  *																																  *
  **********************************************************************************************************************************/
